90dd269d7ec96e950dfc6f94acffab188f2bf8c0
Elif-ozge <138283049+Elif-ozge@users.noreply.github.com>
2024-04-14 16:11:45 +0300

    Add files via upload

---
+++ sensory.py
@@ -0,0 +1,7 @@
+class Sensory:
+    
+    def __init__(self,percept_width,percept_height) :
+        # sensor has a rectangular percept area consist of grid cells
+        self.width=percept_width
+        self.height=percept_height
+    
